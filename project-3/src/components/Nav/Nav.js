889f71796f7c532f8ecd698bee198bfbe3c5d3f9
Andrew <andrewbowden92@gmail.com>
2018-08-31 13:39:52 -0700

    NavBar

--- project-3/src/components/Nav/Nav.js
+++ project-3/src/components/Nav/Nav.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Nav = () => (
 <div id="navLinks">
-  <nav className="container-fluid navbar row">
+  <nav className="container-fluid navbar navbar-expand-lg row">
     <section className="col-sm-2">
       <a className="navbar-brand" id="logo" href="/">  </a>
         <img src="/src/images/logo.png" alt="Placeholder"></img>
     </section>
-    <section className="container col-sm-6 navbar ">
+    <section className="col-sm-6 navbar ">
           <li className="nav-link">
                         <a href="Ingredients.html " className="active "  role="button " data-toggle="button">Bio</a></li>
           <li className="nav-link ">
