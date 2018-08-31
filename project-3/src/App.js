d56b01030f8f30fb8f8eeb983a11ba3bd502d44b
Andrew <andrewbowden92@gmail.com>
2018-08-31 13:14:58 -0700

    NavBar

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />CockTail Mixer
+          <img src={logo} className="App-logo" alt="logo"/>
           </header>
 
           <Nav />
--- project-3/src/components/Nav/Nav.js
+++ project-3/src/components/Nav/Nav.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 const Nav = () => (
-  <div id="navContainer">
-  <nav className="container-fluid navbar">
+<div id="navLinks">
+  <nav className="container-fluid navbar row">
     <section className="col-sm-2">
-      <a className="navbar-brand" id="logo" href="/"> </a>
-        <img src="Logo" alt="logo"></img>
+      <a className="navbar-brand" id="logo" href="/">  </a>
+        <img src="/src/images/logo.png" alt="Placeholder"></img>
     </section>
+    <section className="container col-sm-6 navbar ">
+          <li className="nav-link">
+                        <a href="Ingredients.html " className="active "  role="button " data-toggle="button">Bio</a></li>
+          <li className="nav-link ">
+                        <a href="Recipes.html "  role="button" data-toggle="button">Recipes</a></li>
+          <li className="nav-link ">
+                        <a href="Goodtimes.html " role="button" data-toggle="button">Goodtimes</a></li>
+      </section>
+      <ul className="navbar-nav mr-auto"></ul>
+      <section className="col-sm-2">
+        Saved Drinks
+      </section>
   </nav>
-  </div>
+</div>
+
 
   
 );
